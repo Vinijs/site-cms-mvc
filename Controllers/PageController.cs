dd8483a0da38b7ee012f86fd115a0436ed984ad0
Vinijs <vini-spider-6@hotmail.com>
2024-01-29 02:17:52 -0300

    renderizando html no formato MVC

---
+++ Controllers/PageController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using site_cms_mvc.Models;
+
+namespace site_cms_mvc.Controllers;
+
+public class PageController : Controller
+{
+
+    [Route("/")]
+    public async Task<ActionResult> Index()
+    {
+
+        HttpClient http = new HttpClient();
+        var response = await http.GetAsync($"{Program.ApiHost}/api/paginas/home.json");
+        return render(response);
+    }
+
+    [Route("/{slug}")]
+    public async Task<ActionResult> IndexPaths(string slug)
+    {
+
+        HttpClient http = new HttpClient();
+        var response = await http.GetAsync($"{Program.ApiHost}/api/paginas/{slug}.json");
+        return render(response);        
+    }
+
+    private ContentResult render(HttpResponseMessage response)
+    {
+        if (response.IsSuccessStatusCode)
+        {
+            var result = response.Content.ReadAsStringAsync().Result;
+            var pagina = JsonConvert.DeserializeObject<Pagina>(result);
+            Response.ContentType = "text/html;";
+            return Content(pagina.Conteudo);
+        }
+           return Content("Página não encontrada");
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Linq;
+
+namespace site_cms_mvc
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            JToken jAppSettings = JToken.Parse(File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "appsettings.json")));
+            Program.ApiHost = jAppSettings["host"].ToString();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
